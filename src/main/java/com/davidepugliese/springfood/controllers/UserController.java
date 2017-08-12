ef87b4a298c038926786bc63cdfb3cfed409207f
Davide Pugliese <dvd.pugliese@gmail.com>
2017-08-12 15:48:40 +0300

    implemented Gson

--- src/main/java/com/davidepugliese/springfood/controllers/UserController.java
+++ src/main/java/com/davidepugliese/springfood/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.davidepugliese.springfood.controllers;
 
 import com.davidepugliese.springfood.domain.UserDAO;
 import com.davidepugliese.springfood.models.User;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,8 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.davidepugliese.springfood.services.EncryptionUtilities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/user/{id}")
 public class UserController {
 
     @RequestMapping(method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
@@ -23,14 +27,20 @@ public class UserController {
     }
     @RequestMapping(method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus( HttpStatus.CREATED )
+
     public @ResponseBody
-    Object addUser(@RequestBody User data,  Model model) {
+    String addUser(@RequestBody User data, Model model) {
         User user = new User();
         user.setUsername(data.getUsername());
         user.setPassword(EncryptionUtilities.encryptPassword(data.getPassword()));
         this.userService.saveUser(user);
-        model.addAttribute("user", user);
-        return user;
+//        model.addAttribute("user", user);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "User created successfully");
+        Gson gson = new Gson();
+        return gson.toJson(response);
     }
     @Autowired
     UserDAO userService;
